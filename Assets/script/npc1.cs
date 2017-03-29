7f45952fde58df74dd09ca281826190848cd35ae
lefton22 <lefton22@aliyun.com>
2017-03-28 23:04:23 -0400

    update
    update

---
+++ Assets/script/npc1.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc1 : MonoBehaviour {
+	
+	GameObject myParent;
+	Vector3 v3_parent;
+	Vector3 v3_intersectPoint;
+
+	Vector3 v3_worldPos;
+	GameObject nearestWheel;
+
+	GameObject lw1;
+	GameObject lgo_collide;
+	List<GameObject> lgo_collides;
+
+	float radius_collided;
+
+	bool isChangeTrail;
+
+	float meetTime;
+
+
+
+	void Start () 
+	{
+////find the parent and get the parent's axis
+		myParent =  transform.parent.gameObject;
+
+		lw1 = GameObject.Find ("w1");
+
+		//Debug.Log (" npc1's parent: " + transform.parent.gameObject);
+		isChangeTrail = true;
+
+	}
+	
+
+	void Update ()
+	{
+		float radius = myParent.transform.localScale.x;
+
+		v3_parent = myParent.transform.position;
+
+	
+		//transform.localPosition = new Vector3 (/*parent.transform.localScale.x/4f*/0.5f,0,0);
+
+		////get all the gameobjects collided with this npc's parent?
+
+		if (myParent != lw1)
+		{
+		lgo_collides = myParent.GetComponent<follow_rotate> ().go_collides; 
+		//Debug.Log ("go_cillides: " + lgo_collides[0] + " " + lgo_collides[1] + " " + lgo_collides[2] );
+			//Debug.Log(gameObject + " not w1...");
+		}
+
+//		if (myParent == lw1) 
+//		{
+//		lgo_collides = myParent.GetComponent<follow_rotate> ().go_collides; 
+//		}
+
+
+		for (int i = 0; i < lgo_collides.Count; i++) 
+
+		{ 
+
+////find the collided wheel from the parent wheel, and get the position
+
+			Vector3 v3_lgo_collides = lgo_collides[i].transform.position;
+
+			if (lgo_collides[i] == lw1)
+			{
+				 radius_collided = lgo_collides[i].GetComponent<rotate> ().radius_currentWheel; 
+			}
+
+			if (lgo_collides [i] != lw1) 
+			{
+				 radius_collided = lgo_collides [i].GetComponent<follow_rotate> ().radius_currentWheel; 
+			}
+			//Debug.Log ("v3_parentsNearest: " +v3_parentsNearest + "this parent: " + v3_parent);
+
+////calculate the cross point of two wheels
+
+			Vector3 dist_two =   v3_lgo_collides - v3_parent;
+
+			v3_intersectPoint = v3_parent + dist_two.normalized * /*radius_collided / 2f*/ radius/2f;
+
+//// create a collider at the position of cross point, and if any npc collide with this collider, change tha parent
+			v3_worldPos = transform.TransformPoint (transform.position);
+			//Debug.Log ("v3_worldPos: " + v3_worldPos);
+
+////ctreat only one cross point for every tw crossing wheels
+			/// if the cross point is near the npc,cross
+
+			if (Mathf.Abs (v3_intersectPoint.x - transform.position.x) < 0.2f &&
+				Mathf.Abs (v3_intersectPoint.y - transform.position.y) < 0.2f && isChangeTrail ) 
+			{
+				Transform currentWorldPos = transform;
+
+				transform.SetParent (lgo_collides[i].transform); 
+				myParent = lgo_collides [i];
+
+				transform.position = v3_intersectPoint;
+		
+				isChangeTrail = false;
+
+				meetTime = Time.time;
+
+			}
+
+			if(	Time.time - meetTime > 1.5f) 
+			{
+				isChangeTrail = true;
+			}
+
+			//Debug.Log("isChangeTrial: " +isChangeTrail);
+
+		}
+
+		//Debug.Log ("npc's nearest gameobject is: " + nearestWheel);
+
+//		//if there is no same collision here
+//		//if there is no collider there, then create the collider:
+//		//once: 
+//		GameObject wheel_n = Instantiate(Resources.Load("crosspoint"), v3_intersectPoint, Quaternion.identity) as GameObject; // "w2"is the name in the Resources file,
+//		                                                                                             //mouseV3 is the place which it will be born. 	
+
+	}
+
+
+	///  and if any npc collide with this collider, change tha parent
+
+}
---
+++ Assets/script/rotate.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public float speed = 30f;  // ?how can i set this value on the Inspector?
+	//public float OriRoSpeed;
+
+//this rotating speed is the first energy of this world
+	public float Speed
+	{
+		get{ return speed; }
+		set { speed = value;}
+	} 
+
+    public float timeHere;
+
+	public float radius_currentWheel;
+
+	public GameObject go_collide_w1;
+
+	GameObject lGameManager;
+
+	void Start () 
+	{
+
+		//Speed = OriRoSpeed;
+	}
+
+	void Update () 
+	{
+		radius_currentWheel = transform.localScale.x;
+
+		transform.Rotate (0,0, Time.deltaTime * Speed);
+
+////create a time of this wheel
+		timeHere = timeHere + Time.deltaTime * Speed;
+		//Debug.Log ("TimeHere w1: " + timeHere);
+
+
+		//Debug.Log (GameManager.gear);
+
+		lGameManager = GameObject.Find ("GameManager");
+	}
+		
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "wheel") 
+		{
+			go_collide_w1 = other.gameObject;
+
+			//go_collides_w1.Add (go_collide);
+
+			if (GameManager.gear_collides.Contains (go_collide_w1)) 
+			{
+			} 
+			else 
+			{
+				GameManager.gear_collides.Add (go_collide_w1);
+			}
+
+			//Debug.Log ("collide");
+			lGameManager.SendMessage ("whenListChange");
+
+		}
+
+	}
+}
